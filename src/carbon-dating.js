62cedb44dbc349c73f02e5faff62d728420b5de9
ionovnn <ionovnn@monqlab.com>
2022-10-14 23:51:18 +0300

    feat: add solution for Count cats, Carbon dating and Dream team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(str) {
+  if (
+    typeof str !== 'string'
+    || +str <= 0 
+    || +str >= 15 
+    || str === '' 
+    || isNaN(str) 
+  ) return false;
+  return Math.ceil((Math.log(15 / str)) / (0.693 / 5730));
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  let counter = 0;
+  for (let row of arr) {
+    for (let item of row) {
+      if (item === '^^') counter ++;
+    }
+  }
+  return counter;
 }
 
 module.exports = {
